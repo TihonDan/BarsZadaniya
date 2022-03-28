cd8fd7cb217590f9c3f6da49ff5dd52b391bfc4a
Danila.T <roman-petrov-05@inbox.ru>
2022-03-28 22:36:50 +0300

    Добавьте файлы проекта.

---
+++ 2Задание/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test
+{
+    class Program
+    {
+        static List<Entity> person = new List<Entity>()
+        {
+           new Entity (1, 0, "Root entity"),
+           new Entity (2, 1, "Child of 1 entity"),
+           new Entity (3 , 1,"Child of 1 entity"),
+           new Entity (4, 2, "Child of 2 entity"),
+           new Entity (5, 4, "Child of 4 entity")
+        };
+        static void Main(string[] args)
+        {
+            foreach (var temp in Method1())
+            {
+                var pers = temp;
+                foreach (var dict in pers)
+                {
+                    Console.WriteLine($"{dict.Key} " + "\n" + $"{dict.Value} ");
+                }
+            }
+        }
+
+        public class Entity
+        {
+            public int Id { get; set; }
+            public int ParentId { get; set; }
+            public string Name { get; set; }
+
+            public Entity(int id, int pID, string name)
+            {
+                Name = name;
+                ParentId = pID;
+                Id = id;
+            }
+        }
+
+        static IEnumerable<Dictionary<int, List<Entity>>> Method1()
+        {
+            var dict = new Dictionary<int, List<Entity>>();
+            for (int i = 0; i < person.Count; i++)
+            {
+                List<Entity> temp = person.Where(a => a.ParentId == i).ToList();
+                if (temp != null)
+                {
+                    dict.Add(i, temp);
+                    dict.Remove(3);
+                }
+            }
+            yield return dict;
+        }
+
+    }
+
+}
---
+++ BarsDz/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace _1_Задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileLogger = new LocalFileLogger<string>(@"C:\Bars.txt");
+            fileLogger.LogInfo("Информация о файле");
+            fileLogger.LogWarrning("Опасность");
+            fileLogger.LogError("Ошибка", new Exception());
+
+            var fileLogger1 = new LocalFileLogger<float>(@"C:\Bars.txt");
+            fileLogger1.LogInfo("Information");
+            fileLogger1.LogWarrning("Dangerous");
+            fileLogger1.LogError("Error", new Exception());
+
+            var fileLogger2 = new LocalFileLogger<int>(@"C:\Bars.txt");
+            fileLogger2.LogInfo("Infolrmation or Zadanie");
+            fileLogger2.LogWarrning("Zadanie in warning");
+            fileLogger2.LogError("Zadanie contains Error", new Exception());
+        }
+
+        public interface ILogger
+        {
+            public void LogInfo(string message);
+            public void LogWarrning(string message);
+            public void LogError(string message, Exception ex);
+        }
+
+        public class LocalFileLogger<T> : ILogger
+        {
+            public string Name { get; }
+            public LocalFileLogger(string name)
+            {
+                Name = name;
+            }
+
+            public void LogInfo(string message)
+            {
+                File.AppendAllText(Name, $"[Info]: [{typeof(T).Name}] : {message}" + Environment.NewLine);
+            }
+
+            public void LogWarrning(string message)
+            {
+                File.AppendAllTextAsync(Name, $"[Warning]:[{typeof(T).Name}]:{message}" + Environment.NewLine);
+            }
+
+            public void LogError(string message, Exception ex)
+            {
+                File.AppendAllTextAsync(Name, $"[Error]:[{typeof(T).Name}]:{message}.{ex.Message}");
+            }
+
+        }
+    }
+}
